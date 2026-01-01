2f50f9db4d2a2ccc76bc9c7b341adde9d86066ad
antoniuk-oleksandr <antonyk063@gmail.com>
2026-01-01 15:09:28 +0200

    feat(user-find-by-username-or-email): add user repository findUserByUsernameOrEmail method

--- src/main/java/com/example/blogs/app/api/user/repository/UserRepository.java
+++ src/main/java/com/example/blogs/app/api/user/repository/UserRepository.java
@@ -4,9 +4,12 @@ import com.example.blogs.app.api.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Data access for user entities with unique constraints on username and email.
  */
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findUserByUsernameOrEmail(String username, String email);
 }
