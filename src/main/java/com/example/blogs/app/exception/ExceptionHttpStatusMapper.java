78d9a0505c7f7a084cf3c15e0ea55c05689f9e21
antoniuk-oleksandr <antonyk063@gmail.com>
2026-01-04 18:06:50 +0200

    refactor(jwt-and-exceptions): update classes to match renamed JWT classes and InvalidCredentialsException

--- src/main/java/com/example/blogs/app/exception/ExceptionHttpStatusMapper.java
+++ src/main/java/com/example/blogs/app/exception/ExceptionHttpStatusMapper.java
@@ -1,7 +1,6 @@
 package com.example.blogs.app.exception;
 
 import com.example.blogs.app.api.auth.exception.InvalidCredentialsException;
-import com.example.blogs.app.api.auth.exception.UnauthorizedException;
 import com.example.blogs.app.api.user.exception.EmailTakenException;
 import com.example.blogs.app.api.user.exception.FailedToFindUserException;
 import com.example.blogs.app.api.user.exception.UserNotFoundException;
@@ -23,8 +22,7 @@ public class ExceptionHttpStatusMapper {
             EmailTakenException.class, HttpStatus.CONFLICT,
             UserNotFoundException.class, HttpStatus.NOT_FOUND,
             InvalidCredentialsException.class, HttpStatus.UNAUTHORIZED,
-            FailedToFindUserException.class, HttpStatus.INTERNAL_SERVER_ERROR,
-            UnauthorizedException.class, HttpStatus.UNAUTHORIZED
+            FailedToFindUserException.class, HttpStatus.INTERNAL_SERVER_ERROR
     );
 
     /**
