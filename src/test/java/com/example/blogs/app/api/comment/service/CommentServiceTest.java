e89810fdc422b5b920791d486cb0cf6d15aaf9df
antoniuk-oleksandr <antonyk063@gmail.com>
2026-01-14 21:17:03 +0200

    feat(comments-find-by-post-id): add comments-find-by-post-id support in service

---
+++ src/test/java/com/example/blogs/app/api/comment/service/CommentServiceTest.java
@@ -0,0 +1,60 @@
+package com.example.blogs.app.api.comment.service;
+
+import com.example.blogs.app.api.comment.entity.CommentEntity;
+import com.example.blogs.app.api.comment.repository.adapter.CommentRepositoryAdapter;
+import com.example.blogs.app.api.post.entity.PostEntity;
+import com.example.blogs.app.api.user.entity.UserEntity;
+import com.example.blogs.app.util.TestUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CommentServiceTest {
+
+    @Mock
+    private CommentRepositoryAdapter commentRepositoryAdapter;
+
+    private CommentService commentService;
+
+
+    @BeforeEach
+    void setUp() {
+        commentService = new CommentServiceImpl(commentRepositoryAdapter);
+    }
+
+    @Test
+    void getCommentsByPostId_shouldReturnComments() {
+        LocalDateTime now = LocalDateTime.now().withNano(0);
+        UserEntity mockUser = TestUtils.createMockUserEntity(1L, now);
+        PostEntity mockPost = TestUtils.createMockPostEntity(1L, now, mockUser);
+        List<CommentEntity> mockComments = List.of(
+                TestUtils.createMockCommentEntity(1L, now, mockUser, mockPost),
+                TestUtils.createMockCommentEntity(2L, now, mockUser, mockPost)
+        );
+        when(commentRepositoryAdapter.findAllByPostId(1L)).thenReturn(mockComments);
+
+        List<CommentEntity> actualComments = commentService.getCommentsByPostId(1L);
+
+        assertThat(actualComments).isEqualTo(mockComments);
+        verify(commentRepositoryAdapter).findAllByPostId(1L);
+    }
+
+    @Test
+    void getCommentsByPostId_shouldReturnEmptyListWhenNoComments() {
+        when(commentRepositoryAdapter.findAllByPostId(2L)).thenReturn(List.of());
+
+        List<CommentEntity> actualComments = commentService.getCommentsByPostId(2L);
+
+        assertThat(actualComments).isEmpty();
+        verify(commentRepositoryAdapter).findAllByPostId(2L);
+    }
+}
